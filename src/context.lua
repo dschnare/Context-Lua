b6f63ebdab92939fbea66dc904db3b0d268ac911
dschnare <darren@darrenschnare.com>
2011-10-30 18:54:44 -0400

    Changed how the global context is defined.

--- src/context.lua
+++ src/context.lua
@@ -14,60 +14,63 @@ This means that 'legacy' code can be used alongside contextually aware code.
 local stack = {}
 
 function createContext()
-    local loaded = {}
+  local loaded = {}
 
-    local context = {
-        -- A convenience function to create a new context.
-        create = function()
-            return createContext()
-        end,
-        require = ''
-    }
+  local context = {
+    -- A convenience function to create a new context.
+    create = createContext,
+    -- Reserve space for our contextual 'require'.
+    require = ''
+  }
 
-    context.require = function(modname)
-        -- If a module has already been loaded with the specified name
-        -- in this context then we just return that value immediately.
-        if (loaded[modname]) then return loaded[modname] end
+  context.require = function(modname)
+    -- If a module has already been loaded with the specified name
+    -- in this context then we just return that value immediately.
+    if (loaded[modname]) then return loaded[modname] end
 
-        -- Temporarily clear any module with the same name in the global context.
-        local globalModule = package.loaded[modname]
-        package.loaded[modname] = nil
+    -- Temporarily clear any module with the same name in the global context.
+    local globalModule = package.loaded[modname]
+    package.loaded[modname] = nil
 
-        table.insert(stack, context)
+    table.insert(stack, context)
 
-        -- Attempt to load the module.
-        loaded[modname] = require(modname)
+    -- Attempt to load the module.
+    loaded[modname] = require(modname)
 
-        table.remove(stack)
+    table.remove(stack)
 
-        -- Restore the module with the same name in the global context.
-        package.loaded[modname] = globalModule
+    -- Restore the module with the same name in the global context.
+    package.loaded[modname] = globalModule
 
-        return loaded[modname]
-    end
+    return loaded[modname]
+  end
 
-    return context
+  return context
 end
 
-table.insert(stack, createContext())
+-- Set the current context to the global context.
+table.insert(stack, {
+  create = createContext,
+  require = require
+})
 
 
 return {
-    --[[
-        Returns the context that loaded the current module or the global context.
-        Contexts are tables with the following properties:
-            function require(modname) - Requires the specified module by loading it into the context and returning the result.
-            function create() - Creates a new context.
-    --]]
-    current = function()
-        return stack[#stack]
-    end,
-    --[[
-        Creates a new context. Returns a table with the following properties:
-            function require(modname) - Requires the specified module by loading it into the context and returning the result.
-            function create() - Creates a new context.
-    --]]
-    create = function()
-        return createContext()
-    end
+  --[[
+    Returns the context that loaded the current module or the global context.
+    Contexts are tables with the following properties:
+      function require(modname) - Requires the specified module by loading it into the context and returning the result.
+      function create() - Creates a new context.
+  --]]
+  current = function()
+    return stack[#stack]
+  end,
+  --[[
+    Creates a new context. Returns a table with the following properties:
+      function require(modname) - Requires the specified module by loading it into the context and returning the result.
+      function create() - Creates a new context.
+  --]]
+  create = function()
+      return createContext()
+  end
 }
